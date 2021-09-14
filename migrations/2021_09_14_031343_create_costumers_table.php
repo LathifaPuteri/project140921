d2f405d4b5a00d3266b1b40da6850c754401b0bc
LathifaPuteri <73085162+LathifaPuteri@users.noreply.github.com>
2021-09-14 10:59:07 +0700

    Add files via upload

---
+++ migrations/2021_09_14_031343_create_costumers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCostumersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('costumers', function (Blueprint $table) {
+            $table->bigIncrements('id_pelanggan');
+            $table->string('nama');
+            $table->text('alamat');
+            $table->integer('telp');
+            $table->string('username');
+            $table->string('password');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('costumers');
+    }
+}
